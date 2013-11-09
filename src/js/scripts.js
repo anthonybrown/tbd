e6865dfa6ddc2c394ab1f5dd8d2c5d6259a4e68d
Tony Brown <tony@tonybrownsdesign.com>
2013-11-09 07:20:18 -0500

    changed minor

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,8 +5,6 @@
   $(function () {
     // say hello
     console.log('Tony Brown says hello, and wants to tell you the God loves you.');
-    console.log('Thank you Jesus for saving us from sin!');
-    console.log('testing console');
   });
 
 })(jQuery, window, document);
